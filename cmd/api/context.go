5fc561fbb621a7d4ab959a6697b4cedff85bc9be
Victor Hugo <vhclaudiogomes@gmail.com>
2026-07-02 15:50:37 -0300

    fix: error

--- cmd/api/context.go
+++ cmd/api/context.go
@@ -7,18 +7,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var ErrUserNotFound = errors.New("user not found")
+var ErrUnauthorized = errors.New("user not authenticated")
 
 func (app *application) GetUserFromContext(c *gin.Context) (*database.User, error) {
 	contextUser, exists := c.Get("user")
 
 	if !exists {
-		return nil, ErrUserNotFound
+		return nil, ErrUnauthorized
 	}
 
 	user, ok := contextUser.(*database.User)
 	if !ok {
-		return nil, ErrUserNotFound
+		return nil, ErrUnauthorized
 	}
 
 	return user, nil
